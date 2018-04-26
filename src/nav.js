21ec744809e1a5108da76a1b26591c2ca694292b
pawel misiak <pawelmisiak@live.com>
2018-04-25 21:29:53 -0400

    fixed top menu, cleaned up the code

--- src/nav.js
+++ src/nav.js
@@ -3,29 +3,23 @@ import './nav.css';
 
 const Nav = () => {
   return(
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container">
         <a className="navbar-brand" href="#page-top">Pawel Misiak</a>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i className="fa fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav text-uppercase ml-auto">
-            <li className="nav-item">
+          <ul className="navbar-nav text-uppercase">
+            <li>
               <a className="nav-link" href="#about">About</a>
             </li>
-            <li className="nav-item">
+            <li>
               <a className="nav-link" href="#portfolio">Portfolio</a>
             </li>
-            <li className="nav-item">
+            <li>
               <a className="nav-link" href="#team">Skills</a>
             </li>
-            <li className="nav-item">
+            <li>
               <a className="nav-link" href="#contact">Contact</a>
             </li>
           </ul>
-        </div>
       </div>
     </nav>
   );
