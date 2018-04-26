1ed186e8933d7102f70d7a452e3b4de860cb5807
pawel misiak <pawelmisiak@live.com>
2018-04-26 15:42:01 -0400

    made changes to Portfolio comopnent, now it contains links to specific apps. Will work to fix About component

--- src/portfolio.js
+++ src/portfolio.js
@@ -15,10 +15,10 @@ const Portfolio = () => {
         <img src='https://media.giphy.com/media/8hZri2xwZzxDGZ8bBB/giphy.gif'/>
       </Tilt>
       <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
-        <img src='https://media.giphy.com/media/oHv9d1KM9sSLgKObDL/giphy.gif'/>
+        <a href="https://face-find.herokuapp.com/"><img src="https://media.giphy.com/media/oHv9d1KM9sSLgKObDL/giphy.gif" title="made at imgflip.com"/></a>
       </Tilt>
       <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
-        <img src='https://media.giphy.com/media/TIzgTYtyFmX1fJPObf/giphy.gif'/>
+        <a href="https://robo-friend.herokuapp.com/"><img src='https://media.giphy.com/media/TIzgTYtyFmX1fJPObf/giphy.gif'/></a>
       </Tilt>
     </div>
   </div>
