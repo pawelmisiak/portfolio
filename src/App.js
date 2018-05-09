1945a669ca7cb94b22d3561731c6d9f62d1f309c
pawel misiak <pawelmisiak@live.com>
2018-05-09 10:08:19 -0400

    finish work on the background. Removed gif from about section and will add my profile picture

--- src/App.js
+++ src/App.js
@@ -21,25 +21,34 @@ class App extends Component {
           <Nav />
         </nav> */}
         <section
-          data-background-color='rgb(32, 202, 172)'
+          data-background-color='rgb(94,110,158)'
           className='js-color-stop'>
           <div className="About">
             <About />
           </div>
         </section>
         <section
-          data-background-color='rgb(60, 191, 246)'
+          data-background-color='rgb(139,212,241)'
           className='js-color-stop'>
           <div className="Portfolio">
             <Portfolio />
           </div>
         </section>
-        <div className="Skills">
-          <Skills />
-        </div>
-        <div className="Contact">
-          <Contact />
-        </div>
+        <section
+          data-background-color='rgb(139,224,122)'
+          className='js-color-stop'>
+          <div className="Skills">
+            <Skills />
+          </div>
+        </section>
+        {/* rgb(94,165,80) */}
+        <section
+          data-background-color='rgb(241,203,108)'
+          className='js-color-stop'>
+          <div className="Contact">
+            <Contact />
+          </div>
+        </section>
         <p className="App-footer">
         </p>
       </div>
