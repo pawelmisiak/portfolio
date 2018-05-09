fd1ee79abc0a23f41f49564aa6bd1358700f97cd
pawel misiak <pawelmisiak@live.com>
2018-05-09 16:22:26 -0400

    finished working on the about section with cross platform styling. The buttons lost their coloring. Now working on moving the titles to left and underlying them

--- src/about.js
+++ src/about.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './styles/about.css';
+import { Button } from 'react-bootstrap';
 
 const About = () => {
   return (
-    <div className="AboutMain">
-        <h3 className="h3 tc pa3 pb6">About</h3>
-        <div className="ParagAndPict">
-          <div>
-            <p className="pb3 Paragraph">
-              Programming is like a set of tools that can easily transform into toys and the workplace into playground.<br />
+    <div >
+        <h3 className="h3 tc pa3">About</h3>
+        <div className="halfs">
+          <div className="half">
+            <p className="paragraph">
+              Programming is like a set of tools that can easily transform into toys and the workplace into playground.<br/><br/>
               I am a NYC-based softwere developer with a background in computer science and photography. Both of those skills I find very useful in ideas and design creation.
               I enjoy working in a team and approach problems from unusual perspectives.
             </p>
             <a href="./assets/Resume.pdf" type="button" class="btn btn-outline-warning btn-lg">Resume
             </a>
           </div>
-          <div>
+          <div className="half">
             <img
               src="./assets/profile.jpg" alt="" className="shadow-3 profilePicture"/>
           </div>
