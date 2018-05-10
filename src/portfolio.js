8238c344f8553a35fbc95e3cfd13d1e2c30aef5d
pawel misiak <pawelmisiak@live.com>
2018-05-10 17:57:47 -0400

    compleated working on portfolio decriptions. Now will change around contact component

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,15 +11,28 @@ const Portfolio = () => {
     <div >
       <h3 className="title">Portfolio</h3>
       <div className="Gifs">
-      <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
-        <img src='https://media.giphy.com/media/8hZri2xwZzxDGZ8bBB/giphy.gif'/>
-      </Tilt>
-      <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
-        <a href="https://face-find.herokuapp.com/"><img src="https://media.giphy.com/media/oHv9d1KM9sSLgKObDL/giphy.gif" title="made at imgflip.com"/></a>
-      </Tilt>
-      <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
-        <a href="https://robo-friend.herokuapp.com/"><img src='https://media.giphy.com/media/TIzgTYtyFmX1fJPObf/giphy.gif'/></a>
-      </Tilt>
+        <div className="tile">
+          <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
+            <img src='./assets/project1.gif'/>
+          </Tilt>
+          <h5 className="pb3">Youtube-like app</h5>
+          <p>Re-creation of youtube.com that dynamically
+            searches for relevant videos using youtube's API. The app is build uppon React and Redux what makes it very snappy</p>
+        </div>
+        <div className="tile">
+          <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
+            <a href="https://face-find.herokuapp.com/"><img src="./assets/project2.gif" title="made at imgflip.com"/></a>
+          </Tilt>
+          <h5 className="pb3">Face Find</h5>
+          <p>The app uses external API, designated database, log in/out functionality and interesting dynamic background</p>
+        </div>
+        <div className="tile">
+          <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
+            <a href="https://robo-friend.herokuapp.com/"><img src='./assets/project3.gif'/></a>
+          </Tilt>
+          <h5 className="pb3">Robo Friends</h5>
+          <p>It allows quickly search through the database based on the name</p>
+        </div>
     </div>
   </div>
   );
