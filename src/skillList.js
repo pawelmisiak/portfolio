16dbdf1f37929513b483c6753630ae0d4e37380a
pawel misiak <pawelmisiak@live.com>
2018-05-17 13:52:06 -0400

    refactored skills

---
+++ src/skillList.js
@@ -0,0 +1,66 @@
+export const skills = [
+  {
+    name: 'Git',
+    iconClassName: 'devicon-git-plain colored',
+  },
+  {
+    name: 'JavaScript',
+    iconClassName: 'devicon-javascript-plain colored',
+  },
+  {
+    name: 'Atom',
+    iconClassName: 'devicon-atom-original colored',
+  },
+  {
+    name: 'Bootstrap',
+    iconClassName: 'devicon-bootstrap-plain colored',
+  },
+  {
+    name: 'CSS3',
+    iconClassName: 'devicon-css3-plain colored',
+  },
+  {
+    name: 'Github',
+    iconClassName: 'devicon-github-plain colored',
+  },
+  {
+    name: 'Heroku',
+    iconClassName: 'devicon-heroku-original colored',
+  },
+  {
+    name: 'HTML5',
+    iconClassName: 'devicon-html5-plain-wordmark colored',
+  },
+  {
+    name: 'Linux',
+    iconClassName: 'devicon-linux-plain colored',
+  },
+  {
+    name: 'MySQL',
+    iconClassName: 'devicon-mysql-plain colored',
+  },
+  {
+    name: 'Node.js',
+    iconClassName: 'devicon-nodejs-plain colored',
+  },
+  {
+    name: 'Photoshop',
+    iconClassName: 'devicon-photoshop-plain colored',
+  },
+  {
+    name: 'PostgreSQL',
+    iconClassName: 'devicon-postgresql-plain colored',
+  },
+  {
+    name: 'React',
+    iconClassName: 'devicon-react-original colored',
+  },
+  {
+    name: 'Ruby',
+    iconClassName: 'devicon-ruby-plain colored',
+  },
+  {
+    name: 'Slack',
+    iconClassName: 'devicon-slack-plain colored',
+  },
+]
--- src/skills.js
+++ src/skills.js
@@ -1,124 +1,27 @@
 import React from 'react';
 import './styles/skills.css';
+import {skills} from './skillList';
 
 const Skills = () => {
+
+  const SkillsMapped = skills.map((skill,idx) => {
+    return(
+      <li key={idx}>
+        <span className="icon">
+          <i className={skills[idx].iconClassName}></i>
+        </span>
+        <br />
+        {skills[idx].name}
+      </li>
+    );
+
+  });
+
   return (
     <div className="skills_container">
       <h3 className="title">Skills</h3>
       <ul className="icons_list">
-        <li>
-          <span className="icon">
-            <i className="devicon-git-plain colored"></i>
-          </span>
-          <br />
-          Git
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-javascript-plain colored"></i>
-          </span>
-          <br />
-          JavaScript
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-atom-original colored"></i>
-          </span>
-          <br />
-          Atom
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-bootstrap-plain colored"></i>
-          </span>
-          <br />
-          Bootstrap
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-css3-plain colored"></i>
-          </span>
-          <br />
-          CSS3
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-github-plain colored"></i>
-          </span>
-          <br />
-          Github
-        </li>
-        <li>
-          <span className="icon ">
-            <i className="devicon-heroku-original colored"></i>
-          </span>
-          <br />
-          Heroku
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-html5-plain-wordmark colored"></i>
-          </span>
-          <br />
-          HTML5
-        </li>
-        <br />
-        <li>
-          <span className="icon">
-            <i className="devicon-linux-plain colored"></i>
-          </span>
-          <br />
-          Linux
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-mysql-plain colored"></i>
-          </span>
-          <br />
-          MySQL
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-nodejs-plain colored"></i>
-          </span>
-          <br />
-          Node.js
-        </li>
-        <li>
-          <span className="icon ">
-            <i className="devicon-photoshop-plain colored"></i>
-          </span>
-          <br />
-          Photoshop
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-postgresql-plain colored"></i>
-          </span>
-          <br />
-          PostgreSQL
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-react-original colored"></i>
-          </span>
-          <br />
-          React
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-ruby-plain colored"></i>
-          </span>
-          <br />
-          Ruby
-        </li>
-        <li>
-          <span className="icon">
-            <i className="devicon-slack-plain colored"></i>
-          </span>
-          <br />
-          Slack
-        </li>
+        {SkillsMapped}
       </ul>
     </div>
   );
