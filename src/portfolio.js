1aec065348c97f7d67da0dda1da9da5c393a0eb3
pawel misiak <pawelmisiak@live.com>
2018-05-17 14:11:11 -0400

    finish working on ProjectList file, now will refactor and implement the portfolio component

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,48 +1,17 @@
 import React from 'react';
 import './styles/portfolio.css';
 import Tilt from 'react-tilt'
+import { Projects } from './portfolioList';
 
 const Portfolio = () => {
 
   return (
     <div >
       <h3 className="title">Portfolio</h3>
-      <div className="Gifs">
-        <div className="tile">
-          <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
-            <img src='./assets/project1.gif' alt=""/>
-          </Tilt>
-          <div className="linkButtons">
-            <a href="https://github.com/pawelmisiak/youtube_like_app" className="btn btn-outline-dark btn-lg">GITHUB </a>
-          </div>
-          <h5 className="pb3">Youtube-like app</h5>
-          <p>Re-creation of youtube.com that dynamically
-            searches for relevant videos using youtube's API. The app is build uppon React and Redux what makes it very snappy</p>
-        </div>
-        <div className="tile">
-          <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
-            <img src="./assets/project2.gif" title="made at imgflip.com" alt=""/>
-          </Tilt>
-          <div className="linkButtons">
-            <a href="https://face-find.herokuapp.com/" className="btn btn-outline-dark btn-lg">LIVE </a>
-            <a href="https://github.com/pawelmisiak/face-detection-frontend" className="btn btn-outline-dark btn-lg">GITHUB </a>
-          </div>
-          <h5 className="pb3">Face Find</h5>
-          <p>The app uses external API, designated database, log in/out functionality and interesting dynamic background</p>
-        </div>
-        <div className="tile">
-          <Tilt className="Tilt shadow-1" options={{ max : 7, reverse: true }} >
-            <img src='./assets/project3.gif' alt=""/>
-          </Tilt>
-          <div className="linkButtons">
-            <a href="https://robo-friend.herokuapp.com/" className="btn btn-outline-dark btn-lg">LIVE </a>
-            <a href="https://github.com/pawelmisiak/robots-dynamic-search" className="btn btn-outline-dark btn-lg">GITHUB </a>
-          </div>
-          <h5 className="pb3">Robo Friends</h5>
-          <p>It allows quickly search through the database based on the name</p>
-        </div>
+      <ul className="Gifs">
+
+      </ul>
     </div>
-  </div>
   );
 }
 
---
+++ src/portfolioList.js
@@ -0,0 +1,24 @@
+export const Projects = [
+  {
+    imageSrc: './assets/project1.gif',
+    linkGit: 'https://github.com/pawelmisiak/youtube_like_app',
+    linkLive: '',
+    title: 'Youtube-like app',
+    description: `Re-creation of youtube.com that dynamically
+      searches for relevant videos using youtube's API. The app is build uppon React and Redux what makes it very snappy`,
+  },
+  {
+    imageSrc: './assets/project2.gif',
+    linkLive: 'https://face-find.herokuapp.com/',
+    linkGit: 'https://github.com/pawelmisiak/face-detection-frontend',
+    title: 'Face Find',
+    description: `The app uses external API, designated database, log in/out functionality and interesting dynamic background`,
+  },
+  {
+    imageSrc: './assets/project3.gif',
+    linkLive: 'https://robo-friend.herokuapp.com/',
+    linkGit: 'https://github.com/pawelmisiak/robots-dynamic-search',
+    title: 'Robo Friends',
+    description: `It allows quickly search through the database based on the name`,
+  },
+]
